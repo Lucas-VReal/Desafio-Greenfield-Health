1d97240cc00768abb4f5fc474a2de4b0ded151b2
Lucas Real <96266377+Lucas-VReal@users.noreply.github.com>
2022-08-23 10:05:16 -0300

    Commit inicial do Backend

---
+++ Greenfield-Health-Backend/src/main/java/br/com/GreenfieldHealth/GreenfieldHealthApplication.java
@@ -0,0 +1,13 @@
+package br.com.GreenfieldHealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenfieldHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenfieldHealthApplication.class, args);
+	}
+
+}
---
+++ Greenfield-Health-Backend/src/test/java/br/com/GreenfieldHealth/GreenfieldHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.GreenfieldHealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenfieldHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
